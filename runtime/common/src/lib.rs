9e44607dd3af210aac423e8a8bea94d60e9322f2
Abhishek Shah <abhishekshah3@gmail.com>
2023-06-01 14:09:35 +0530

    split runtimes into devnet/mainnet

---
+++ runtime/common/src/lib.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+use sp_runtime::Perbill;
+
+use frame_support::weights::{constants::WEIGHT_REF_TIME_PER_SECOND, Weight};
+
+// Cumulus types re-export
+// These types are shared between the mainnet and devnet runtimes
+//https://github.com/paritytech/cumulus/tree/master/parachains/common
+pub use parachains_common::{
+	impls::DealWithFees, AccountId, AuraId, Balance, BlockNumber, Hash, Index, Signature,
+};
+
+/// This determines the average expected block time that we are targeting.
+/// Blocks will be produced at a minimum duration defined by `SLOT_DURATION`.
+/// `SLOT_DURATION` is picked up by `pallet_timestamp` which is in turn picked
+/// up by `pallet_aura` to implement `fn slot_duration()`.
+///
+/// Change this to adjust the block time.
+pub const MILLISECS_PER_BLOCK: u64 = 12000;
+
+// NOTE: Currently it is not possible to change the slot duration after the chain has started.
+// Attempting to do so will brick block production.
+pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
+
+// Time is measured by number of blocks.
+pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
+pub const HOURS: BlockNumber = MINUTES * 60;
+pub const DAYS: BlockNumber = HOURS * 24;
+
+/// We assume that ~5% of the block weight is consumed by `on_initialize` handlers. This is
+/// used to limit the maximal weight of a single extrinsic.
+pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
+
+/// We allow `Normal` extrinsics to fill up the block up to 75%, the rest can be used by
+/// `Operational` extrinsics.
+pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
+
+/// We allow for 0.5 of a second of compute with a 12 second average block time.
+pub const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
+	WEIGHT_REF_TIME_PER_SECOND.saturating_div(2),
+	polkadot_primitives::MAX_POV_SIZE as u64,
+);
